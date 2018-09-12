fd7cbc47aca6d532e2af07e07a250388ea9f0faf
Jacky Chan <cjacky624@gmail.com>
2018-09-12 19:44:04 -0400

    First commit for Lab1Part2

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any date(mm/dd/year): ");
+            DateTime firstDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another date(mm/dd/year): ");
+            DateTime secondDate = DateTime.Parse(Console.ReadLine());
+
+            Dates(firstDate, secondDate);
+        }
+        public static void Dates(DateTime firstDate, DateTime secondDate)
+        {
+            TimeSpan diff = firstDate - secondDate;
+
+            Console.WriteLine("Days : " + Math.Abs(diff.TotalDays));
+            Console.WriteLine("Hours : " + Math.Abs(diff.TotalHours));
+            Console.WriteLine("Minutes : " + Math.Abs(diff.TotalMinutes));
+        }
+    }
+}
